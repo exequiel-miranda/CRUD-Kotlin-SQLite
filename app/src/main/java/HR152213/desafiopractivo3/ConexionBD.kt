c1893d8f93156371a9ad98a6914b6616300ce2a2
Bryan <bryan_miranda@ricaldone.edu.sv>
2024-05-13 22:21:28 -0600

    insert complete

--- app/src/main/java/HR152213/desafiopractivo3/ConexionBD.kt
+++ app/src/main/java/HR152213/desafiopractivo3/ConexionBD.kt
@@ -8,15 +8,14 @@ import android.database.sqlite.SQLiteOpenHelper
 class ConexionBD(context: Context): SQLiteOpenHelper(context, nombreBD, factory, version) {
 
     companion object{
-        internal val nombreBD = "Compras"
+        internal val nombreBD = "dbCompras"
         internal val factory = null
         internal val version = 1
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE lista_compra( id_lista int, fecha varchar(50), titulo varchar(50))")
-        db?.execSQL("CREATE TABLE productos_lista( id_producto_lista int PRIMARY KEY AUTOINCREMENT, id_lista int, producto varchar(50) )")
-        db?.execSQL("ALTER TABLE productos_lista ADD FOREIGN KEY (id_lista) REFERENCES lista_compra(id_lista)")
+        db?.execSQL("CREATE TABLE lista_compra( id_lista INTEGER PRIMARY KEY AUTOINCREMENT, fecha varchar(50), titulo varchar(50))")
+        db?.execSQL("CREATE TABLE productos_lista( id_producto_lista INTEGER PRIMARY KEY AUTOINCREMENT, id_lista INTEGER NOT NULL, producto varchar(50), FOREIGN KEY (id_lista) REFERENCES lista_compra(id_lista))")
     }
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
--- app/src/main/java/HR152213/desafiopractivo3/MainActivity.kt
+++ app/src/main/java/HR152213/desafiopractivo3/MainActivity.kt
@@ -2,11 +2,28 @@ package HR152213.desafiopractivo3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var conexion: ConexionBD
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtFecha = findViewById<EditText>(R.id.txtFecha)
+        val txtTitulo = findViewById<EditText>(R.id.txtTitulo)
+        val btnGuardar = findViewById<Button>(R.id.btnGuardar)
+
+        conexion = ConexionBD(this)
+
+        btnGuardar.setOnClickListener {
+            conexion.CrearLista(txtFecha.text.toString(), txtTitulo.text.toString())
+            Toast.makeText(this, "Lista de compras guardados", Toast.LENGTH_LONG).show()
+        }
     }
+
+
 
 }
